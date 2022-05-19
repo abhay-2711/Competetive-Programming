0c715dd233bf634dea2b580a83b87f912f3d8742
Abhay <abhaypratap.s20@iiits.in>
2022-05-19 22:08:21 +0530

    Chef and Ballons

---
+++ Practice/Chef and Balllons.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define lint long long int
+
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        lint r,g,b;
+        cin>>r>>g>>b;
+        lint k;
+        cin>>k;
+        lint c,d,e;
+        c=min(r,min(g,b));
+        e=max(r,max(g,b));
+        d=r+g+b-c-e;
+        if(k<=c){
+            cout<<3*k-2<<endl;
+        }
+        else if(k>c && k<=d){
+            cout<<3*c+2*(k-c)-1<<endl;
+        }
+        else if(k>d && k<=e){
+            cout<<d+e+k<<endl;
+        }
+        else if(k==e){
+	        cout<<r+g+b;
+	    }
+    }
+}
